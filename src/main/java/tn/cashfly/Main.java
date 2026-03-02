772d0d6d8f0998d2ac5f771adf2f2c413755633b
mohamederg25 <mohamed.erguez@esprit.tn>
2026-03-02 17:50:48 +0100

    2.2V

--- src/main/java/tn/cashfly/Main.java
+++ src/main/java/tn/cashfly/Main.java
@@ -16,7 +16,6 @@ public class Main extends Application {
         primaryStage.setMinWidth(1024);
         primaryStage.setMinHeight(600);
         primaryStage.show();
-        
     }
 
     public static void main(String[] args) {
