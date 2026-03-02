9197a6ce7814602446c2b40f78fd8541ee4fdb5a
souhir <souhir.meddeb@esprit.tn>
2026-03-02 23:26:25 +0100

    metiers

--- gestion_entreprises/src/main/java/com/example/gestion_entreprises/controllers/Notification.java
+++ gestion_entreprises/src/main/java/com/example/gestion_entreprises/controllers/Notification.java
@@ -49,12 +49,12 @@ public class Notification extends StackPane {
     }
 
     private void initialize() {
-        cross.getStyleClass().add("small-hyperlink-label");
+        cross.getStyleClass().add("small-hyperlink-label");//kkkk
         cross.setOnMouseClicked(e -> onRemove.run());
     }
 
     public void setOnRemove(Runnable onRemove) {
         this.onRemove = onRemove;
-        //initialisation1231aaaaaaa
+
     }
 }
